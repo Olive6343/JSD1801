a135cf48f3daa2a6fefc937c2ed282aff3cd2fa7
Olive6343 <1655771702@qq.com>
2018-05-23 19:00:14 +0800

    提交20180409项目

---
+++ 20180409/src/com/ldq/test/Test.java
@@ -0,0 +1,18 @@
+package com.ldq.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a = 1000;
+		boolean flag1 = true;
+		if(a == 1000)
+			flag1 = false;
+		System.out.println(flag1);
+		
+		boolean flag2 = true;
+		int b = 100;
+		if(b == 100)
+			flag2 = false;
+		System.out.println(flag2);
+	}
+}
